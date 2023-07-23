4d00c27bd1365ea2d196750ad2247cf2dd5ea31c
Sumeet Parmar <z.sumeet@gmail.com>
2023-07-23 18:14:20 -0500

    added top level tests - all are failing

---
+++ transformer_test.go
@@ -0,0 +1,22 @@
+package transformer
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func TestPNGConverstion(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestJPEGConverstion(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestWEBPConverstion(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
