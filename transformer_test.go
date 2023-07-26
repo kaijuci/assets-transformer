2482cedc5240ba7d86d2a3f6f75d72a67024ba3e
Sumeet Parmar <z.sumeet@gmail.com>
2023-07-26 16:09:48 -0500

    added jpeg output support

--- transformer_test.go
+++ transformer_test.go
@@ -57,7 +57,35 @@ func TestPNGConversion(t *testing.T) {
 }
 
 func TestJPEGConversion(t *testing.T) {
-	// t.Errorf("Test not implemented")
+	at, err := NewAssetTransformer()
+	if err != nil {
+		t.Errorf("error creating transformer: %v", err)
+	}
+
+	outfile := fmt.Sprintf("/tmp/%s", "face.jpeg")
+	result, err := at.Transform(facePNG, &TransformOption{Format: JPEG, Size: AssetSize{Width: 100, Height: 100}, Outfile: outfile})
+	if err != nil {
+		t.Fatalf("error transforming asset: %v", err)
+	}
+
+	if len(*result) == 0 {
+		t.Fatalf("result is empty")
+	}
+
+	size, format, err := getImageSizeAndFormat(*result)
+	if err != nil {
+		t.Fatalf("error getting image size and format: %v", err)
+	}
+
+	if size.Width != 100 || size.Height != 100 {
+		t.Fatalf("expected size 100x100, got %v", *size)
+	}
+
+	if *format != JPEG {
+		t.Fatalf("expected format png, got %s", *format)
+	}
+
+	t.Logf("Result: %s size: %dx%d format: %s", *result, size.Width, size.Height, *format)
 }
 
 func TestWEBPConversion(t *testing.T) {
