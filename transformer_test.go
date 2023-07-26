d5c19756fddf4117cc03a1219c81021448cf3e66
Sumeet Parmar <shipit@users.noreply.github.com>
2023-07-26 16:32:27 -0500

    [feature] GIF output format #9

--- transformer_test.go
+++ transformer_test.go
@@ -120,6 +120,38 @@ func TestWEBPConversion(t *testing.T) {
 	t.Logf("Result: %s size: %dx%d format: %s", *result, size.Width, size.Height, *format)
 }
 
+func TestGIFConversion(t *testing.T) {
+	at, err := NewAssetTransformer()
+	if err != nil {
+		t.Errorf("error creating transformer: %v", err)
+	}
+
+	outfile := fmt.Sprintf("/tmp/%s", "face.gif")
+	result, err := at.Transform(facePNG, &TransformOption{Format: GIF, Size: AssetSize{Width: 100, Height: 100}, Outfile: outfile})
+	if err != nil {
+		t.Fatalf("error transforming asset: %v", err)
+	}
+
+	if len(*result) == 0 {
+		t.Fatalf("result is empty")
+	}
+
+	size, format, err := getImageSizeAndFormat(*result)
+	if err != nil {
+		t.Fatalf("error getting image size and format: %v", err)
+	}
+
+	if size.Width != 100 || size.Height != 100 {
+		t.Fatalf("expected size 100x100, got %v", *size)
+	}
+
+	if *format != GIF {
+		t.Fatalf("expected format png, got %s", *format)
+	}
+
+	t.Logf("Result: %s size: %dx%d format: %s", *result, size.Width, size.Height, *format)
+}
+
 func getImageSizeAndFormat(filename string) (*AssetSize, *AssetFormat, error) {
 	reader, err := os.Open(filename)
 	if err != nil {
