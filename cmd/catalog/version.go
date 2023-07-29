15d75b487c7ff536cf29825e7d7253e66e0f987f
Sumeet Parmar <shipit@users.noreply.github.com>
2023-07-29 15:29:11 -0500

    [chore] version cmd #14

---
+++ cmd/catalog/version.go
@@ -0,0 +1,17 @@
+package catalog
+
+import (
+	"log"
+
+	"github.com/kaijuci/assets-transformer/vars"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of xfrmr",
+	Long:  `All software has versions. This is xfrmr's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Printf("version: %s\n", vars.Version)
+	},
+}
