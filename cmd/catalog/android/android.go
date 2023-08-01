e62d8fab0522bee21588375ca57e235e1859d181
Sumeet Parmar <z.sumeet@gmail.com>
2023-07-31 22:06:33 -0500

    generalized core work loop

--- cmd/catalog/android/android.go
+++ cmd/catalog/android/android.go
@@ -1,7 +1,6 @@
 package android
 
 import (
-	"errors"
 	"strings"
 
 	"github.com/kaijuci/transformer"
@@ -30,39 +29,28 @@ const (
 	`
 )
 
-type androidAssetWorkFn func(inputFilename, outputDir, assetName, format string) ([]*string, error)
-
 type androidWork struct {
-	mapFn         map[string]androidAssetWorkFn
 	inputFilename string
 	outputDir     string
 	assetName     string
-	types         []string
-	format        string
+	iconTypes     []android.AndroidIconType
+	format        transformer.AssetFormat
 }
 
 func newAndroidAssetWork(inputFilename, outputDir, assetName, types, format string) *androidWork {
 	return &androidWork{
-		mapFn: map[string]androidAssetWorkFn{
-			"launcher": handleLauncherAssetType,
-			"all":      handleAllAssetTypes,
-		},
 		inputFilename: inputFilename,
 		outputDir:     outputDir,
 		assetName:     assetName,
-		types:         strings.Split(types, ","),
-		format:        format,
+		iconTypes:     resolveIconTypes(strings.Split(types, ",")),
+		format:        transformer.AssetFormat(strings.ToLower(format)),
 	}
 }
 
 func (w *androidWork) doWork() ([]*string, error) {
 	var result []*string
-	for _, t := range w.types {
-		fn, ok := w.mapFn[t]
-		if !ok {
-			return nil, errors.New(ErrInvalidAssetType)
-		}
-		paths, err := fn(w.inputFilename, w.outputDir, w.assetName, w.format)
+	for _, t := range w.iconTypes {
+		paths, err := w.transform(t)
 		if err != nil {
 			return nil, err
 		}
@@ -73,31 +61,38 @@ func (w *androidWork) doWork() ([]*string, error) {
 	return result, nil
 }
 
-func handleLauncherAssetType(inputFilename, outputDir, assetName, format string) ([]*string, error) {
-	at, err := transformer.NewAndroidAssetTransformer(outputDir)
-	if err != nil {
-		return nil, err
+func resolveIconTypes(types []string) []android.AndroidIconType {
+	var result []android.AndroidIconType
+	for _, t := range types {
+		i := android.AndroidIconType(t)
+		result = append(result, i)
 	}
 
-	opt := transformer.AndroidTransformOption{
-		IconType: android.AndroidIconTypeLauncher,
-		Format:   transformer.AssetFormat(strings.ToLower(format)),
-	}
+	return result
+}
 
-	gen, err := at.TransformAsset(inputFilename, assetName, &opt)
+func (w *androidWork) transform(iconType android.AndroidIconType) ([]*string, error) {
+	at, err := transformer.NewAndroidAssetTransformer(w.outputDir)
 	if err != nil {
 		return nil, err
 	}
 
 	result := []*string{}
+
+	opt := transformer.AndroidTransformOption{
+		IconType: iconType,
+		Format:   w.format,
+	}
+
+	gen, err := at.TransformAsset(w.inputFilename, w.assetName, &opt)
+	if err != nil {
+		return nil, err
+	}
+
 	for _, v := range gen {
 		path := v
 		result = append(result, &path)
 	}
 
 	return result, nil
-}
-
-func handleAllAssetTypes(inputFilename, outputDir, assetName, format string) ([]*string, error) {
-	return nil, errors.New("not implemented")
 }
